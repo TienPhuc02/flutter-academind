e04f1b644d5e735558d9934a704cf8704d63e50f
TienPhuc02 <dtp2352002@gmail.com>
2023-12-13 11:38:10 +0700

    building custom widget class

---
+++ flutter_chapter_2/lib/function.dart
@@ -0,0 +1,7 @@
+int add({num1, num2}) {
+  return num1 + num2;
+}
+
+void main() {
+  print(add(num1: 10, num2: 10));
+}
--- flutter_chapter_2/lib/main.dart
+++ flutter_chapter_2/lib/main.dart
@@ -5,21 +5,28 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blue,
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                colors: [Colors.blue, Colors.green],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight),
-          ),
-          child: const Center(
-            child: Text(
-              "Hello World!!",
-              style: TextStyle(color: Colors.white, fontSize: 28),
-            ),
-          ),
-        ), //widget -> layout
+        body: GradientContainer(), //widget -> layout
       ), //scaffold class,
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override // đơn giản là thêm chức năng vào hàm build của StatelessWidget
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            colors: [Colors.blue, Colors.green],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight),
+      ),
+      child: const Center(
+        child: Text(
+          "Hello World!!",
+          style: TextStyle(color: Colors.white, fontSize: 28),
+        ),
+      ),
+    );
+  }
 }
