fa6cf5b1e530f887601e6fe19f4427574b93b928
TienPhuc02 <dtp2352002@gmail.com>
2023-12-13 14:06:57 +0700

    introducing variable

--- flutter_chapter_2/lib/gradient_container.dart
+++ flutter_chapter_2/lib/gradient_container.dart
@@ -1,16 +1,19 @@
 import "package:flutter/material.dart";
 import "package:flutter_chapter_2/text_container.dart";
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({Key? key}) : super(key: key);
   @override // đơn giản là thêm chức năng vào hàm build của StatelessWidget
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-            colors: [Colors.blue, Colors.green],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight),
+            colors: const [Colors.blue, Colors.green],
+            begin: startAlignment,
+            end: endAlignment),
       ),
       child: const Center(child: TextContainer()),
     );
