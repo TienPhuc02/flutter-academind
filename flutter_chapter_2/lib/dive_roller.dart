481cedf24ce7b2f7c6077eacc8af08b21ed9f671
TienPhuc02 <dtp2352002@gmail.com>
2023-12-13 16:22:42 +0700

    generating random number

--- flutter_chapter_2/lib/dive_roller.dart
+++ flutter_chapter_2/lib/dive_roller.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import "dart:math";
+
+final randomizer = Random();
 
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
@@ -9,10 +12,10 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  var activeDiceImage = "assets/images/dice-1.png";
+  var currentDiceNumber = 2;
   void rollDice() {
     setState(() {
-      activeDiceImage = "assets/images/dice-2.png";
+      currentDiceNumber = randomizer.nextInt(6) + 1;
     });
   }
 
@@ -23,7 +26,7 @@ class _DiceRollerState extends State<DiceRoller> {
       children: [
         Center(
           child: Image.asset(
-            activeDiceImage,
+            "assets/images/dice-$currentDiceNumber.png",
             width: 200.0,
           ),
         ),
