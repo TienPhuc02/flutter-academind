86b01c5ae29298292e7b6a7444f59d15ccb524f1
TienPhuc02 <dtp2352002@gmail.com>
2023-12-13 11:59:35 +0700

    working with constructor super

--- flutter_chapter_2/lib/main.dart
+++ flutter_chapter_2/lib/main.dart
@@ -2,7 +2,7 @@ import "package:flutter/material.dart";
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blue,
         body: GradientContainer(), //widget -> layout
@@ -12,7 +12,7 @@ void main() {
 }
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer({Key? key}) : super(key: key);
   @override // đơn giản là thêm chức năng vào hàm build của StatelessWidget
   Widget build(context) {
     return Container(
@@ -31,3 +31,6 @@ class GradientContainer extends StatelessWidget {
     );
   }
 }
+/*
+ const GradientContainer({Key? key}) : super(key: key); là một constructor được khai báo cho class GradientContainer. Constructor này chấp nhận một tham số có tên key (để xác định key của widget) và truyền giá trị của tham số này cho constructor của lớp cha (super(key: key))
+ */
