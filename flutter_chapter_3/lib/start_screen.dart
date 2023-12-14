0975a832ec0c34f7bb93013274acc18bfd904b2e
TienPhuc02 <dtp2352002@gmail.com>
2023-12-14 10:10:42 +0700

    adding tranparent to widget

--- flutter_chapter_3/lib/start_screen.dart
+++ flutter_chapter_3/lib/start_screen.dart
@@ -10,8 +10,8 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(context) {
     return Container(
-      constraints: const BoxConstraints
-          .expand(), // Mở rộng Container để fill toàn bộ không gian của body
+      // constraints: const BoxConstraints
+      //     .expand(), // Mở rộng Container để fill toàn bộ không gian của body
       decoration: const BoxDecoration(
         gradient: LinearGradient(
             begin: Alignment.topLeft,
@@ -21,39 +21,48 @@ class StartScreen extends StatelessWidget {
               Color.fromARGB(255, 201, 86, 221)
             ]),
       ),
-      child: Container(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 100,
-            ),
-            Image.asset(
-              "assets/images/quiz-logo.png",
-              width: 250.0,
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            TextClass(
+      child: Column(
+        // crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(
+            height: 100,
+          ),
+          Image.asset(
+            "assets/images/quiz-logo.png",
+            width: 300.0,
+            color: Color.fromARGB(174, 255, 255, 255),
+          ),
+          // Opacity(
+          //   opacity: 0.4,
+          //   child: Image.asset(
+          //     "assets/images/quiz-logo.png",
+          //     width: 250.0,
+          //   ),
+          // ),
+          const SizedBox(
+            height: 50,
+          ),
+          Center(
+            child: TextClass(
               text: "Learn Flutter the fun Way!",
               styleText: TextStyle(color: Colors.white, fontSize: 25),
             ),
-            const SizedBox(
-              height: 50,
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          TextButton.icon(
+            onPressed: handleClickStartQuiz,
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(fontSize: 20),
             ),
-            TextButton.icon(
-              onPressed: handleClickStartQuiz,
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(fontSize: 20),
-              ),
-              icon: const Icon(Icons.arrow_right_alt),
-              label: TextClass(
-                text: "Start Quizz",
-              ),
-            )
-          ],
-        ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: TextClass(
+              text: "Start Quizz",
+            ),
+          )
+        ],
       ),
     );
   }
