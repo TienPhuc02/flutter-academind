53d9815c4bba58adb725969747b7b56fd4ae34ac
TienPhuc02 <dtp2352002@gmail.com>
2023-12-20 14:35:19 +0700

    showing a date  picker

--- flutter_chapter_5/lib/widgets/new_expense.dart
+++ flutter_chapter_5/lib/widgets/new_expense.dart
@@ -11,6 +11,17 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+
+  void _presentDatePicker() {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
+  }
+
   // var _enteredTitle = "";
   // void _saveTitleInput(String inputVale) {
   //   _enteredTitle = inputVale;
@@ -37,14 +48,38 @@ class _NewExpenseState extends State<NewExpense> {
             ),
             // keyboardType: ,
           ),
-          TextField(
-            controller: _amountController,
-            maxLength: 50,
-            decoration: const InputDecoration(
-              prefixText: '\đ ',
-              label: Text("Amount"),
-            ),
-            keyboardType: TextInputType.number,
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  maxLength: 50,
+                  decoration: const InputDecoration(
+                    prefixText: '\đ ',
+                    label: Text("Amount"),
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text("Selected Date"),
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(
+                        Icons.calendar_month,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
           Row(
             children: [
