f47df3a026158c0ac44cfe31ed53719db1c20724
TienPhuc02 <dtp2352002@gmail.com>
2023-12-21 10:51:31 +0700

    creating a fullscreen modal

--- flutter_chapter_5/lib/widgets/new_expense.dart
+++ flutter_chapter_5/lib/widgets/new_expense.dart
@@ -57,11 +57,14 @@ class _NewExpenseState extends State<NewExpense> {
       );
       return;
     }
-    widget.onAddExpense(Expense(
-        date: _selectedDate!,
-        title: _titleController.text,
-        amount: enterdAmount,
-        category: _selectedCategory));
+    widget.onAddExpense(
+      Expense(
+          date: _selectedDate!,
+          title: _titleController.text,
+          amount: enterdAmount,
+          category: _selectedCategory),
+    );
+    Navigator.pop(context);
   }
 
   @override
@@ -74,7 +77,7 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
       child: Column(
         children: [
           TextField(
