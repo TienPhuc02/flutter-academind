7cf7b77b8934a8c9ba67ee5651de53fb07eaf8e8
TienPhuc02 <dtp2352002@gmail.com>
2023-12-22 10:47:18 +0700

    text theme

--- flutter_chapter_5/lib/main.dart
+++ flutter_chapter_5/lib/main.dart
@@ -22,6 +22,25 @@ class MyApp extends StatelessWidget {
             backgroundColor: kColorScheme.onPrimaryContainer,
             foregroundColor: kColorScheme.primaryContainer,
           ),
+          cardTheme: const CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
+            ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+                // titleLarge:ThemeData().textTheme.copyWith() ,
+                titleLarge: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    color: kColorScheme.onSecondaryContainer,
+                    fontSize: 14),
+              ),
         ),
         home: const Expenses());
   }
